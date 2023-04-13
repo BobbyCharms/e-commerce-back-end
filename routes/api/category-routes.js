a99c86e0bba5a143c34d94aa1dc6d5e9e2c00a26
BobbyCharms <robertcharming@Gmail.com>
2023-04-13 08:11:36 -0400

    adds find one catagory route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,13 +16,26 @@ router.get('/', async (req, res) => {
   }  
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+// find one category by its `id` value
+// be sure to include its associated Products
+router.get("/:id", async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category is associated with that ID' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+// create a new category
 router.post('/', (req, res) => {
-  // create a new category
+  
 });
 
 router.put('/:id', (req, res) => {
