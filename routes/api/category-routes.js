91a7d36b9364056a537155b4365ab3e7f9c4d429
BobbyCharms <robertcharming@Gmail.com>
2023-04-13 08:16:43 -0400

    adds post route for catagories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -34,8 +34,13 @@ router.get("/:id", async (req, res) => {
 });
 
 // create a new category
-router.post('/', (req, res) => {
-  
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
